49ba5b8dec7f9da5a958d6d14bb845ecff4a0aee
tropotek <info@tropotek.com>
2016-08-30 13:32:50 +1000

    Fixed the event list manager - Fixed table array handling - Added Html Cell rendderer for the Table lib

---
+++ Tk/Plugin/PluginManager.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tk\Plugin;
+
+
+/**
+ * @author Michael Mifsud <info@tropotek.com>
+ * @link http://www.tropotek.com/
+ * @license Copyright 2016 Michael Mifsud
+ */
+class PluginManager
+{
+
+
+
+
+
+}
