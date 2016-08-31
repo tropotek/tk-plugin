738bed16b3293a570abee567328eaa13bbb078a7
tropotek <info@tropotek.com>
2016-09-01 08:28:06 +1000

    -

---
+++ Bootstrap.php
@@ -0,0 +1,60 @@
+<?php
+namespace plugin;
+
+
+//use Monolog\Handler\StreamHandler;
+//use Monolog\Logger;
+//use Psr\Log\NullLogger;
+
+
+/**
+ * Class Bootstrap
+ *
+ * I am using the composer.json file to auto execute this file using the following entry:
+ *
+ * ~~~json
+ *   "autoload":  {
+ *     "psr-0":  {
+ *       "":  [
+ *         "src/"
+ *       ]
+ *     },
+ *     "files" : [
+ *       "Bootstrap.php"    <-- This one
+ *     ]
+ *   }
+ * ~~~
+ *
+ *
+ * @author Michael Mifsud <info@tropotek.com>  
+ * @link http://www.tropotek.com/  
+ * @license Copyright 2015 Michael Mifsud  
+ */
+class Bootstrap
+{
+
+    /**
+     * This will also load dependant objects into the config, so this is the DI object for now.
+     *
+     */
+    static function execute()
+    {
+        if (version_compare(phpversion(), '5.4.0', '<')) {
+            // php version must be high enough to support traits
+            throw new \Exception('Your PHP5 version must be greater than 5.4.0 [Curr Ver: '.phpversion().']');
+        }
+        
+        // Do not call \Tk\Config::getInstance() before this point
+        $config = \Tk\Config::getInstance();
+        
+
+        
+        return $config;
+    }
+
+}
+
+// called by autoloader, see composer.json -> "autoload" : files []...
+Bootstrap::execute();
+
+var_dump('plugin');
