b70af8cc35b461fd7c2c830cf16cf84eb6f2a0bc
tropotek <info@tropotek.com>
2022-08-11 08:32:27 +1000

    ~Auto: Commit

--- Tk/Plugin/Factory.php
+++ Tk/Plugin/Factory.php
@@ -5,7 +5,7 @@ use Tk\ConfigTrait;
 use Tk\EventDispatcher\EventDispatcher;
 
 /**
- * @author Michael Mifsud <info@tropotek.com>
+ * @author Michael Mifsud <http://www.tropotek.com/>
  * @see http://www.tropotek.com/
  * @license Copyright 2016 Michael Mifsud
  */
